25c11110ce3e75ebeb4cb785d8d87e208c1d3c15
Yang <didtdjwns111@gmail.com>
2023-03-10 20:34:09 +0900

    feat: PreAuthorize Annotation

--- src/main/java/Infleard/security/controller/indexController.java
+++ src/main/java/Infleard/security/controller/indexController.java
@@ -3,6 +3,8 @@ package Infleard.security.controller;
 import Infleard.security.repository.UserRepository;
 import Infleard.security.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,5 +59,17 @@ public class indexController {
         System.out.println(user);
         userRepository.save(new User(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()), user.getEmail(), ROLE_USER));
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터 정보";
     }
 }
