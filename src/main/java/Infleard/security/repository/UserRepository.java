6ca526b01b21227698bfe64fb633eebd2d464543
Yang <didtdjwns111@gmail.com>
2023-03-10 19:58:15 +0900

    feat: add findByUsername Method

--- src/main/java/Infleard/security/repository/UserRepository.java
+++ src/main/java/Infleard/security/repository/UserRepository.java
@@ -1,7 +1,9 @@
-package Infleard.security.config;
+package Infleard.security.repository;
 
 import Infleard.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUsername(String username);
 }
