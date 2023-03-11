92e771daba574a6c74798e180495f2cd6668bc9a
Yang <didtdjwns111@gmail.com>
2023-03-11 14:51:00 +0900

    feat: add PrincipalOauth2UserService

---
+++ src/main/java/Infleard/security/config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,24 @@
+package Infleard.security.config.oauth;
+
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+
+    // oauth2 로 부터 받은 userRequest 데이터에 대한 후처리 함수
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        System.out.println("userRequest.getClientRegistration : " + userRequest.getClientRegistration());
+        System.out.println("userRequest.getAccessToken : " + userRequest.getAccessToken());
+        System.out.println("userRequest.getAdditionalParameters : " + userRequest.getAdditionalParameters());
+        System.out.println("userRequest.getAttributes : " + super.loadUser(userRequest).getAttributes());
+
+        return super.loadUser(userRequest);
+    }
+}
+
